972a0b92c6ed20c67822e3a3feb28d00396b42af
Lauren Krieger <lkrieger104@gmail.com>
2022-11-16 20:28:45 -0800

    fix

--- index.js
+++ index.js
@@ -14,8 +14,9 @@
     let bg_width = 769.15;
     let bg_height = 258.30;
     let door_width = 62.40;
-    let door_start = (bg_width/2) + 43
+    let door_start_horiz = (bg_width/2) + 43;
     let door_padding = 4;
+    let door_start_vert = (bg_height/2) + 14;
 
     let parent = document.getElementById("background");
 
@@ -23,13 +24,13 @@
       let door = document.createElement("img");
       door.src = "resources/shapes/566.svg";
       door.style.width = `${door_width / bg_width * 100}%`;
-      door.style.left = `${(door_start + (door_width+door_padding)*i) / bg_width * 100}%`;
+      door.style.left = `${(door_start_horiz + (door_width+door_padding)*i) / bg_width * 100}%`;
       door.classList.add("doors");
       if (i % 2 === 1) {
         door.classList.add("flip");
       }
       parent.appendChild(door);
-      door.style.top = `${143.2 / bg_height * 100}%`;
+      door.style.top = `${door_start_vert / bg_height * 100}%`;
     }
   }
 
